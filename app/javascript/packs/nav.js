07edeeee504de09de25a9e9627e36ae4a3b9d034
gitlassian <nialldeely@gmail.com>
2020-12-04 18:02:10 +0000

    css fixes 1

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -1,7 +1,3 @@
-//change active status
-$(".nav li a").click(function() {
-    $(this).parent().addClass('active').siblings().removeClass('active');
-    $("#navbar").removeClass("in");    
-});
+
   
   
